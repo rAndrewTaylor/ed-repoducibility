f8f68aee84ae93c56912adbd7fe4e9551606f28c
rAndrewTaylor <r.andrew.taylor@gmail.com>
2020-03-26 09:35:56 -0400

    cleaned file structure

--- src/fig1.R
+++ src/fig1.R
@@ -4,3 +4,10 @@ require(tidyverse)
 require(xlsx)
 
 df <- read("data/journal_articles_mod.xls")
+
+
+df %>%
+  select(open_code_yn:code_avail) %>%
+  gather(key, value) %>%
+  mutate(value = ifelse(value == 'N', 0, 1)) %>%
+  xtabs(value ~ key, data = .)
